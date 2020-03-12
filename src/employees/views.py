387f374d31c84f5a74ff09c1c9adfa100a2533b0
karan <karan.shgl96@gmail.com>
2020-03-12 21:48:20 +0530

    Added default auth

--- src/employees/views.py
+++ src/employees/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required
+def dashboard(request):
+    return render(request, 'base.html', {"data": "Hello World"})

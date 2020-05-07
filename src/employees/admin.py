6663d23afc57b8da08591c7cea1f1186470457b8
vinx-2105 <2017csb1119@iitrpr.ac.in>
2020-05-07 20:30:44 +0530

    fixed employees/admin.py and added first_name, last_name to profile admin page

--- src/employees/admin.py
+++ src/employees/admin.py
@@ -6,9 +6,22 @@ from .models import Profile, Role
 class ProfileAdminModel(admin.ModelAdmin):
 	""" Admin Model """
 
-	list_display = ["user"]
+	def user_email(obj):
+		return obj.user.email.lower()
+	user_email.short_description = 'Email'
 
-	search_fields = ["user", "user.email"]
+	def user_first_name(obj):
+		return obj.user.first_name
+	user_first_name.short_description = 'First Name'
+
+	def user_last_name(obj):
+		return obj.user.last_name
+	user_last_name.short_description = 'Last Name'
+
+
+	list_display = ("user", user_email, user_first_name, user_last_name)
+
+	search_fields = ("user__username", "user__email",  )
 
 	class Meta:
 		model = Profile
@@ -19,9 +32,9 @@ admin.site.register(Profile, ProfileAdminModel)
 class RoleAdminModel(admin.ModelAdmin):
 	""" Admin Model """
 
-	list_display = ["role_name"]
+	list_display = ("role_name", )
 
-	search_fields = ["role_name"]
+	search_fields = ("role_name", )
 
 	class Meta:
 		model = Role
